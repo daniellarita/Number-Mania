3a4ee3d2c871889cbfc08214d4f94788c65ff341
Danni Baxter <dbaxter@wework.com>
2016-08-01 16:44:32 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Number-Mania
-# Number-Mania
+# Number Guessing Mania
+
+Number Guessing Mania
+
+An intense and exciting number guessing game unlike any other. I built this to help me learn how to put together a very, VERY simple web app - and it did!
---
+++ application.js
@@ -0,0 +1,28 @@
+var myGame = function(){
+    // User choice
+    var userChoice = prompt("Type in your first guess here to begin:");
+    // Computer choice
+    var randomNumber = Math.floor(Math.random()*10+1);
+
+    // Compare user choice vs computer choice
+    var counter = 4; //# of tries the user has to win the game - 5 total
+    var result=''; //the result that we will display to the user
+
+    for (var i=counter;i>=0;i--){
+       if (randomNumber == userChoice){
+        result="You win :) The winning number was "+randomNumber;
+        counter=-1;
+      } else if (counter===0){
+        result="Sorry, you're out of tries :( The winning number was "+randomNumber;
+        counter=-1;
+      } else if (randomNumber < userChoice){
+        userChoice = prompt("Nope, try to guess a lower number!");
+        counter--;
+      } else if (randomNumber > userChoice){
+        userChoice = prompt("Nope, try to guess a higher number!");
+        counter--;
+      }
+
+    }
+    alert(result);
+  }
